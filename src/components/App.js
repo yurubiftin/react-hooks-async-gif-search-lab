62cb69af8c526b9a4af01885d52bca6f61f5f54f
yurubahmed <yurubiftin2002@gmail.com>
2023-01-27 11:18:38 -0500

    done

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,18 @@
-import React from "react";
+import React from 'react'
 
-import NavBar from "./NavBar";
+import NavBar from './NavBar'
 
-// the App component should render out the GifListContainer component
+// the App component should render out the GifListContainer component 
+import GifListContainer from './GifListContainer'
 
 function App() {
   return (
     <div>
-      <NavBar color="black" title="Giphy Search" />
+      
+      <NavBar color='black' title="Giphy Search" />
+      <GifListContainer />
     </div>
-  );
+  )
 }
 
 export default App;
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -10,7 +10,9 @@ function NavBar({ color, title }) {
     <nav className={`navbar ${colors[color]}`}>
       <div className="container-fluid">
         <div className="navbar-header">
-          <span className="navbar-brand">{title}</span>
+          <span className="navbar-brand">
+            {title}
+            </span>
         </div>
       </div>
     </nav>
